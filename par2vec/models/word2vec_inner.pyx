88b505619ce9d53eb0196447965d5d9e660aa1b5
Raymond Wong <rayman_407@yahoo.com>
2016-03-03 00:49:28 -0800

    Fixed some errors with importing functions from par2vec_inner. Error possibly due to older version of C compiler.

--- par2vec/models/word2vec_inner.pyx
+++ par2vec/models/word2vec_inner.pyx
@@ -28,12 +28,18 @@ REAL = np.float32
 
 DEF MAX_SENTENCE_LEN = 10000
 
-cdef scopy_ptr scopy=<scopy_ptr>PyCObject_AsVoidPtr(fblas.scopy._cpointer)  # y = x
-cdef saxpy_ptr saxpy=<saxpy_ptr>PyCObject_AsVoidPtr(fblas.saxpy._cpointer)  # y += alpha * x
-cdef sdot_ptr sdot=<sdot_ptr>PyCObject_AsVoidPtr(fblas.sdot._cpointer)  # float = dot(x, y)
-cdef dsdot_ptr dsdot=<dsdot_ptr>PyCObject_AsVoidPtr(fblas.sdot._cpointer)  # double = dot(x, y)
-cdef snrm2_ptr snrm2=<snrm2_ptr>PyCObject_AsVoidPtr(fblas.snrm2._cpointer)  # sqrt(x^2)
-cdef sscal_ptr sscal=<sscal_ptr>PyCObject_AsVoidPtr(fblas.sscal._cpointer) # x = alpha * x
+cdef scopy_ptr scopy
+scopy=<scopy_ptr>PyCObject_AsVoidPtr(fblas.scopy._cpointer)  # y = x
+cdef saxpy_ptr saxpy
+saxpy=<saxpy_ptr>PyCObject_AsVoidPtr(fblas.saxpy._cpointer)  # y += alpha * x
+cdef sdot_ptr sdot
+sdot=<sdot_ptr>PyCObject_AsVoidPtr(fblas.sdot._cpointer)  # float = dot(x, y)
+cdef dsdot_ptr dsdot
+dsdot=<dsdot_ptr>PyCObject_AsVoidPtr(fblas.sdot._cpointer)  # double = dot(x, y)
+cdef snrm2_ptr snrm2
+snrm2=<snrm2_ptr>PyCObject_AsVoidPtr(fblas.snrm2._cpointer)  # sqrt(x^2)
+cdef sscal_ptr sscal
+sscal=<sscal_ptr>PyCObject_AsVoidPtr(fblas.sscal._cpointer) # x = alpha * x
 
 DEF EXP_TABLE_SIZE = 1000
 DEF MAX_EXP = 6
